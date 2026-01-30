2cacbc667d2abe6abfacbd006f85dc525c02b0cf
Atabay Bashimov <migooface27@gmail.com>
2026-01-30 19:28:21 +0100

    week 6

--- sumlist.py
+++ sumlist.py
@@ -1,28 +1,19 @@
 class SumList:
     def __init__(self):
-        # TODO
+        self.sums = []
+        self.current = 0
 
     def append(self, number):
-        # TODO
+        self.sums.append(self.current + number)
+        self.current += number
 
     def sum(self, a, b):
-        # TODO
+        return self.sums[b] - self.sums[a-1] if a-1 >= 0 else self.sums[b]
 
 if __name__ == "__main__":
     numbers = SumList()
-
-    numbers.append(1)
-    numbers.append(2)
-    numbers.append(3)
-    numbers.append(4)
-    numbers.append(5)
-
-    print(numbers.sum(0, 4)) # 15
-    print(numbers.sum(1, 1)) # 2
-    print(numbers.sum(1, 3)) # 9
-    print(numbers.sum(2, 3)) # 7
-    print(numbers.sum(0, 3)) # 10
-
-    numbers.append(1)
-    print(numbers.sum(0, 5)) # 16
-    print(numbers.sum(5, 5)) # 1
+    total = 0
+    for i in range(10**5):
+        numbers.append(i + 1)
+        total += numbers.sum(i // 2, i)
+    print(total) # 125005000050000
